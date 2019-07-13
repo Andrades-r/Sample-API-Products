9b34594f77a2e55d4b5d349e4f3d3829887b7f00
Andrades-r <renato.emanuel.andrades@gmail.com>
2019-07-12 21:05:17 -0300

    iniciando o projeto

---
+++ api/models/product.js
@@ -0,0 +1,9 @@
+const moongose = require('mongoose');
+
+const productSchema = moongose.Schema({
+    _id:moongose.Schema.Types.ObjectId,
+    name: {type: String, required: true},
+    price: {type: Number, required: true}
+});
+
+module.exports = moongose.model('Product',productSchema);
